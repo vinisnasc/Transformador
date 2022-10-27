e83a34b367f647b467e44f9c3ae8d6d4743372f9
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2022-10-27 20:54:55 -0300

    Implementação docker

--- Transformador.API/Program.cs
+++ Transformador.API/Program.cs
@@ -25,17 +25,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile) ; 
     c.IncludeXmlComments(xmlPath);
 });
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+/*if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{*/
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
